40bd5dfefcd3a823697cc0a800eaa7c8b9bc2988
Killerkoen <koen.kraaijveld@gmail.com>
2021-03-18 23:23:16 +0100

    Added very basic starter code for CompoundTagPrinter class

---
+++ src/main/java/util/CompoundTagPrinter.java
@@ -0,0 +1,19 @@
+package util;
+
+import nbt.CompoundTag;
+
+public class CompoundTagPrinter {
+    private CompoundTag compoundTag;
+
+    public CompoundTagPrinter(CompoundTag compoundTag) {
+        this.compoundTag = compoundTag;
+    }
+
+    public String stringify() {
+        return null;
+    }
+
+    public void print() {
+        // TBD
+    }
+}
