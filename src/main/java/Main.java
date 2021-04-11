7901c4720a003dc9fa1ddc3bb7aa850d9144bf3b
Killerkoen <koen.kraaijveld@gmail.com>
2021-04-11 15:58:36 +0200

    Merge branch 'developer'
    # Conflicts:
    #	.gradle/6.7/executionHistory/executionHistory.bin
    #	.gradle/6.7/executionHistory/executionHistory.lock
    #	.gradle/6.7/fileHashes/fileHashes.bin
    #	.gradle/6.7/fileHashes/fileHashes.lock
    #	.gradle/6.7/javaCompile/classAnalysis.bin
    #	.gradle/6.7/javaCompile/javaCompile.lock
    #	.gradle/6.7/javaCompile/taskHistory.bin
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/buildOutputCleanup/outputFiles.bin
    #	.gradle/checksums/checksums.lock
    #	.gradle/checksums/md5-checksums.bin
    #	.gradle/checksums/sha1-checksums.bin
    #	.idea/misc.xml
    #	build.gradle
    #	build/libs/MinecraftFileReader-1.0-SNAPSHOT.jar
    #	build/tmp/compileJava/source-classes-mapping.txt

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        MCAReader reader = new MCAReader("r.0.0.mca");
-        MCAFile mcaFile = reader.readMCAFile();
-        System.out.println(mcaFile.toString());
-    }
-}
